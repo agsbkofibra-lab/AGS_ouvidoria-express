ba3364a95fe0c004448a5a50a35ba7c18b3ff481
Douglas <douglasantonny@hotmail.com>
2026-05-15 11:19:56 -0300

    fix: build vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-  },
-  vite: {
-    nitro: {
-      preset: "vercel",
-    },
-  },
+  plugins: [
+    tanstackStart({
+      server: { entry: "src/server.ts" },
+    }),
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
 });
